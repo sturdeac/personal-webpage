4af1889f9710fb1f66aefc0af3c3a1c3697e2570
sturdeac <sturdeac@rose-hulman.edu>
2019-07-25 19:39:41 -0400

    adding skills

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import Splash from "./components/Splash";
 import Experience from "./components/Experience";
-import Skills from "./components/Skills"
+import Skills from "./components/Skills";
 import "./App.css";
 
 function App() {
   return (
     <div>
       <Splash />
-      <Experience />
-      <Skills />
+      <div class="content">
+        <Experience />
+        <Skills />
+      </div>
     </div>
   );
 }
--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import { Grid, Typography, Container } from "@material-ui/core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHashtag, faJs } from "@fortawesome/free-solid-svg-icons";
+import { faHashtag } from "@fortawesome/free-solid-svg-icons";
+import { faJsSquare, faAngular, faHtml5, faCss3Alt, faNodeJs, faJava, faPython } from "@fortawesome/free-brands-svg-icons";
 import "./styles.css";
 
 export default class Skills extends Component {
@@ -24,31 +25,87 @@ export default class Skills extends Component {
               </Typography>
             </Grid>
             <Grid item>
-              <Grid
-                container
-                direction="row"
-                justify="center"
-                alignItems="center"
-                spacing={10}
-              >
-                <div className="typography">
-                  <Grid item sm={12} md={6} lg={3}>
-                    <FontAwesomeIcon
-                      style={this.iconColor}
-                      icon={faHashtag}
-                      size="5x"
-                    />
-                    <Typography variant="h6" style={this.textColor}>C#/.NET</Typography>
-                  </Grid>
-                  <Grid item sm={12} md={6} lg={3}>
-                    <FontAwesomeIcon
-                      style={this.iconColor}
-                      icon={faJs}
-                      size="5x"
-                    />
-                    <Typography variant="h6" style={this.textColor}>C#/.NET</Typography>
-                  </Grid>
-                </div>
+              <Grid container justify="center" alignItems="center" spacing={10}>
+                <Grid item sm={6} md={3} className="typography">
+                  <FontAwesomeIcon
+                    style={this.iconColor}
+                    icon={faHashtag}
+                    size="5x"
+                  />
+                  <Typography variant="h6" style={this.textColor}>
+                    C#/.NET
+                  </Typography>
+                </Grid>
+                <Grid item sm={6} md={3} className="typography">
+                  <FontAwesomeIcon
+                    style={this.iconColor}
+                    icon={faJsSquare}
+                    size="5x"
+                  />
+                  <Typography variant="h6" style={this.textColor}>
+                    Javascript
+                  </Typography>
+                </Grid>
+                <Grid item sm={6} md={3} className="typography">
+                  <FontAwesomeIcon
+                    style={this.iconColor}
+                    icon={faNodeJs}
+                    size="5x"
+                  />
+                  <Typography variant="h6" style={this.textColor}>
+                    NodeJS
+                  </Typography>
+                </Grid>
+                <Grid item sm={6} md={3} className="typography">
+                  <FontAwesomeIcon
+                    style={this.iconColor}
+                    icon={faAngular}
+                    size="5x"
+                  />
+                  <Typography variant="h6" style={this.textColor}>
+                    AngularJS
+                  </Typography>
+                </Grid>
+                <Grid item sm={6} md={3} className="typography">
+                  <FontAwesomeIcon
+                    style={this.iconColor}
+                    icon={faHtml5}
+                    size="5x"
+                  />
+                  <Typography variant="h6" style={this.textColor}>
+                    HTML5
+                  </Typography>
+                </Grid>
+                <Grid item sm={6} md={3} className="typography">
+                  <FontAwesomeIcon
+                    style={this.iconColor}
+                    icon={faCss3Alt}
+                    size="5x"
+                  />
+                  <Typography variant="h6" style={this.textColor}>
+                    CSS
+                  </Typography>
+                </Grid>
+                <Grid item sm={6} md={3} className="typography">
+                  <FontAwesomeIcon
+                    style={this.iconColor}
+                    icon={faJava}
+                    size="5x"
+                  />
+                  <Typography variant="h6" style={this.textColor}>
+                    Java
+                  </Typography>
+                </Grid>
+                <Grid item sm={6} md={3} className="typography">
+                  <FontAwesomeIcon
+                    style={this.iconColor}
+                    icon={faPython}
+                    size="5x"
+                  />
+                  <Typography variant="h6" style={this.textColor}>
+                    Python
+                  </Typography>
+                </Grid>
               </Grid>
             </Grid>
           </Grid>
